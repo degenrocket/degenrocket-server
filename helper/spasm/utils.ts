4aa1caf4a2931332a7029c8f93d73265b22a664e
degenrocket <noreply@degenrocket.space>
2024-02-11 01:13:18 +0000

    fix: handle errors during hex to bech32 conversion

--- helper/spasm/utils.ts
+++ helper/spasm/utils.ts
@@ -12,18 +12,23 @@ export const isObjectWithValues = (val: any): boolean => {
 
 // Nostr
 export const convertHexToBech32 = (hexKey, prefix?) => {
-  // Convert private or public key from HEX to bech32
-  let bytes = new Uint8Array(hexKey.length / 2);
+  try {
+    // Convert private or public key from HEX to bech32
+    let bytes = new Uint8Array(hexKey.length / 2);
 
-  for(let i = 0; i < hexKey.length; i+=2) {
-      bytes[i/2] = parseInt(hexKey.substr(i, 2), 16);
+    for(let i = 0; i < hexKey.length; i+=2) {
+        bytes[i/2] = parseInt(hexKey.substr(i, 2), 16);
+    }
+
+    const words = bech32.toWords(bytes);
+
+    prefix = prefix ?? 'npub'
+
+    const bech32Key = bech32.encode(prefix, words);
+
+    return bech32Key
+  } catch (error) {
+    console.error(error)
+    return ''
   }
-
-  const words = bech32.toWords(bytes);
-
-  prefix = prefix ?? 'npub'
-
-  const bech32Key = bech32.encode(prefix, words);
-
-  return bech32Key
 }
