512aac98e974b654e64ae9a8b90a10184dfbd518
degenrocket <noreply@degenrocket.space>
2024-03-01 22:00:40 +0000

    chore: clean console.log

--- helper/spasm/custom/customSpasmSources.example.ts
+++ helper/spasm/custom/customSpasmSources.example.ts
@@ -1,10 +1,3 @@
-// Override console.log for production
-if (process.env.NODE_ENV !== "dev") {
-  console.log = () => {}
-  console.info = () => {}
-  console.warn = () => {}
-}
-
 export const spasmSourcesFrequencyHigh =
   [
     // ====================================
