8756bc857d85fd9e9c21ca79aea61c4b5fdb2e4e
degenrocket <noreply@degenrocket.space>
2025-04-11 01:07:11 +0100

    pm2: add more env vars to config

--- ecosystem-prod.config.js
+++ ecosystem-prod.config.js
@@ -5,18 +5,18 @@ module.exports = {
   apps: [
     {
       // name: `prod-back@${packageJson.version}`,
-      name: 'dr-prod-back',
+      name: process.env.BACKEND_PM2_PROD_NAME || 'dr-prod-back',
       script: './dist/api/index.js',
       exec_mode: process.env.BACKEND_PM2_PROD_EXEC_MODE || 'cluster',
       // The number of instances is currently set to '1' because
-      // otherwise bree jobs to fetch RSS/SPASM will be called multiple
-      // times on multiple CPU cores.
-      // TODO: find a better solution, e.g. centralize the fetching job
-      // to a single instance or a separate service.
+      // otherwise bree jobs to fetch RSS/SPASM will be called
+      // multiple times on multiple CPU cores.
+      // TODO: find a better solution, e.g. centralize the
+      // fetching job to a single instance or a separate service.
       instances: process.env.BACKEND_PM2_PROD_INSTANCES || '1',
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: process.env.BACKEND_PM2_PROD_MAX_MEMORY_RESTART || '1G',
       env: {
         NODE_ENV: "production",
         BACKEND_PORT: parseInt(process.env.BACKEND_PROD_PORT) || 5000,
--- ecosystem-stage.config.js
+++ ecosystem-stage.config.js
@@ -5,13 +5,13 @@ module.exports = {
   apps: [
     {
       // name: `stage-back@${packageJson.version}`,
-      name: 'dr-stage-back',
+      name: process.env.BACKEND_PM2_STAGE_NAME || 'dr-stage-back',
       script: './dist/api/index.js',
       exec_mode: process.env.BACKEND_PM2_STAGE_EXEC_MODE || 'cluster',
       instances: process.env.BACKEND_PM2_STAGE_INSTANCES || '1',
       autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      max_memory_restart: process.env.BACKEND_PM2_STAGE_MAX_MEMORY_RESTART || '1G',
       env: {
         NODE_ENV: "staging",
         BACKEND_PORT: parseInt(process.env.BACKEND_STAGE_PORT) || 5000,
